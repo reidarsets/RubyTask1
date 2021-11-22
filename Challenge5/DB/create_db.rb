6be380dd0c19dd0d61f9eaac12379bcbb5887550
reidarsets <Reidarsets@gmail.com>
2021-11-22 14:01:21 +0200

    Challenge 5
    Challenge 5 done (Fixture Report)

---
+++ Challenge5/DB/create_db.rb
@@ -0,0 +1,51 @@
+require_relative './connect_db'
+
+CONN.exec (
+  'CREATE TABLE IF NOT EXISTS offices (
+    "id" SERIAL PRIMARY KEY,
+    "title" varchar NOT NULL,
+    "address" varchar NOT NULL,
+    "city" varchar NOT NULL,
+    "state" varchar NOT NULL,
+    "phone" varchar NOT NULL,
+    "lob" varchar NOT NULL,
+    "type" varchar NOT NULL
+  );'
+)
+
+CONN.exec(
+  'CREATE TABLE IF NOT EXISTS "zones" (
+    "id" SERIAL PRIMARY KEY,
+    "type" varchar NOT NULL,
+    "office_id" int NOT NULL REFERENCES "offices" ("id")
+  );'
+)
+
+CONN.exec(
+  'CREATE TABLE IF NOT EXISTS "rooms" (
+    "id" SERIAL PRIMARY KEY,
+    "name" varchar NOT NULL,
+    "area" int NOT NULL,
+    "max_people" int NOT NULL,
+    "zone_id" int NOT NULL REFERENCES "zones" ("id")
+  );'
+)
+
+CONN.exec(
+  'CREATE TABLE IF NOT EXISTS "fixtures" (
+    "id" SERIAL PRIMARY KEY,
+    "name" varchar NOT NULL,
+    "type" varchar NOT NULL,
+    "room_id" int NOT NULL REFERENCES "rooms" ("id")
+  );'
+)
+
+CONN.exec(
+  'CREATE TABLE IF NOT EXISTS "materials" (
+    "id" SERIAL PRIMARY KEY,
+    "name" varchar NOT NULL,
+    "type" varchar NOT NULL,
+    "cost" int NOT NULL,
+    "fixture_id" int NOT NULL REFERENCES "fixtures" ("id")
+  );'
+)
